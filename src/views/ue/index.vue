8474db073efc236cc20fe49cd7160ea02ddbb07f
pengjiazhen <jzpenga@gmail.com>
2019-10-08 21:55:30 +0800

    update

--- src/views/ue/index.vue
+++ src/views/ue/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <iframe ref="ueIframe"
+    <iframe style="width: 100%;height: 600px"
+            ref="ueIframe"
             id="iframe"
             :src="baseUrl+'/analystui/?lan=en#/project'">
 
@@ -17,7 +18,12 @@
             }
         },
         mounted() {
+            //<iframe id="iframe" onload='document.getElementById("iframe").src="javascript:$(\"#username\").val(\"admin\");$(\"#_password\").val(\"cdsf@119\");$(\"#cas_submit\").click();"' src="/analystui/?lan=en#/project">
+
+        //</iframe>
             const ueIframe = this.$refs['ueIframe'];
+            console.log(ueIframe);
+            ueIframe.src = 'javascript:document.getElementById("username").value(\'admin\');document.getElementById("_password").value(\'cdsf@119\');document.getElementById("cas_submit").click();'
         }
     }
 </script>
