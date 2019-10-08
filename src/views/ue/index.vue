21965675b13acdbf21ce244803f3a1355a955806
pengjiazhen <jzpenga@gmail.com>
2019-10-08 22:19:43 +0800

    update

--- src/views/ue/index.vue
+++ src/views/ue/index.vue
@@ -1,6 +1,7 @@
 <template>
     <iframe style="width: 100%;height: 600px"
             ref="ueIframe"
+            id="ueIframe"
             @onload="initIframe"
             :src="baseUrl+'/analystui/?lan=en#/project'">
 
@@ -21,9 +22,23 @@
             //<iframe id="iframe" onload='document.getElementById("iframe").src="javascript:$(\"#username\").val(\"admin\");$(\"#_password\").val(\"cdsf@119\");$(\"#cas_submit\").click();"' src="/analystui/?lan=en#/project">
 
         //</iframe>
-            const ueIframe = this.$refs['ueIframe'];
+            //const ueIframe = this.$refs['ueIframe'];
             //ueIframe.src = 'javascript:document.getElementById("username").value(\'admin\');console.log(document.getElementById("username"));document.getElementById("_password").val(\'cdsf@119\');document.getElementById("cas_submit").click();'
-            console.log(ueIframe);
+            //console.log(ueIframe);
+            const self = this
+            this.$nextTick(() =>{
+                const iframe = document.querySelector('#ueIframe');
+                if (iframe.attachEvent) {
+                    iframe.attachEvent("onload", function() {
+                        console.log('哈哈哈')
+                    });
+                } else {
+                    iframe.onload = function() {
+                        console.log('IEii')
+                    };
+                }
+            })
+
         },
         methods:{
             initIframe(){
