b09fb0c8e6c7675b42f2274ff41e5476adfc1707
pengjiazhen <jzpenga@gmail.com>
2019-10-10 20:57:31 +0800

    update

--- src/api/ue.js
+++ src/api/ue.js
@@ -16,7 +16,7 @@ export function fetchUrl(data) {
     // request interceptor
     axios.interceptors.request.use(
         config => {
-            console.log(getUEToken());
+            console.log(getUEToken().name);
             if (getUEToken()) {
                 config.headers['name'] = getUEToken().name;
                 config.headers['value'] = getUEToken().value
