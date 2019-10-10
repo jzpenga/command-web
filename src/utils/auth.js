e94b9b619b0f1e4801e505fc9ae9eaf1990ec84e
pengjiazhen <jzpenga@gmail.com>
2019-10-10 19:56:19 +0800

    update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -23,7 +23,7 @@ export function loginUe() {
       var html = ajax.responseText;
       var form1 = html.substring(html.indexOf("<form"), html.indexOf("</form") + 7 );
       console.log(form1);
-      if (form1){
+      if (form1 !== '<!DO'){
         var div = document.createElement("div");
         div.innerHTML = form1;
         var lt = div.children[0].lt.value;
