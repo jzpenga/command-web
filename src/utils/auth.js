e6c341b4a80df13b72acb824cc5d181ffa5e0a6c
pengjiazhen <jzpenga@gmail.com>
2019-10-10 19:52:24 +0800

    update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -21,26 +21,30 @@ export function loginUe() {
   ajax.onreadystatechange = function () {
     if (ajax.readyState===4 &&ajax.status===200) {
       var html = ajax.responseText;
-      console.log(html);
       var form1 = html.substring(html.indexOf("<form"), html.indexOf("</form") + 7 );
-      var div = document.createElement("div");
-      div.innerHTML = form1;
-      var lt = div.children[0].lt.value;
-      var execution = div.children[0].execution.value;
-      var ajax1 = new XMLHttpRequest();
-      ajax1.open('post','/cas/login?locale=zh_CN');
-      ajax1.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-      ajax1.send("password=319a5d2ce2e52aaec8c09008ee85517f&lt=" + lt + "&execution=" + execution + "&username=admin&captcha=1&_eventId=submit");
-      ajax1.onreadystatechange = function () {
-        if (ajax1.readyState===4 &&ajax1.status===200) {
-          // var iframe = document.createElement("iframe");
-          // iframe.src = "/analystui/?lan=en#/project";
-          // iframe.style.display = "none";
-          // document.body.appendChild(iframe);
-          getUeToken();
+      if (form1){
+        var div = document.createElement("div");
+        div.innerHTML = form1;
+        var lt = div.children[0].lt.value;
+        var execution = div.children[0].execution.value;
+        var ajax1 = new XMLHttpRequest();
+        ajax1.open('post','/cas/login?locale=zh_CN');
+        ajax1.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+        ajax1.send("password=319a5d2ce2e52aaec8c09008ee85517f&lt=" + lt + "&execution=" + execution + "&username=admin&captcha=1&_eventId=submit");
+        ajax1.onreadystatechange = function () {
+          if (ajax1.readyState===4 &&ajax1.status===200) {
+            // var iframe = document.createElement("iframe");
+            // iframe.src = "/analystui/?lan=en#/project";
+            // iframe.style.display = "none";
+            // document.body.appendChild(iframe);
+            getUeToken();
 
+          }
         }
+      } else {
+        getUeToken();
       }
+
     }
   }
 }
