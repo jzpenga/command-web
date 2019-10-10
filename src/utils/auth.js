a8bf374ac0b02aba9ba72e5f3b88b973733de5a2
pengjiazhen <jzpenga@gmail.com>
2019-10-10 19:42:23 +0800

    update

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -13,3 +13,48 @@ export function setToken(token) {
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
+
+export function loginUe() {
+  var ajax = new XMLHttpRequest();
+  ajax.open('get','/cas/login?locale=zh_CN');
+  ajax.send();
+  ajax.onreadystatechange = function () {
+    if (ajax.readyState===4 &&ajax.status===200) {
+      var html = ajax.responseText;
+      var form1 = html.substring(html.indexOf("<form"), html.indexOf("</form") + 7 );
+      var div = document.createElement("div");
+      div.innerHTML = form1;
+      var lt = div.children[0].lt.value;
+      var execution = div.children[0].execution.value;
+      var ajax1 = new XMLHttpRequest();
+      ajax1.open('post','/cas/login?locale=zh_CN');
+      ajax1.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+      ajax1.send("password=319a5d2ce2e52aaec8c09008ee85517f&lt=" + lt + "&execution=" + execution + "&username=admin&captcha=1&_eventId=submit");
+      ajax1.onreadystatechange = function () {
+        if (ajax1.readyState===4 &&ajax1.status===200) {
+          // var iframe = document.createElement("iframe");
+          // iframe.src = "/analystui/?lan=en#/project";
+          // iframe.style.display = "none";
+          // document.body.appendChild(iframe);
+          getUeToken();
+
+        }
+      }
+    }
+  }
+}
+
+export function getUeToken() {
+  const ajax = new XMLHttpRequest();
+  ajax.open('get', '/analystui/?lan=en#/project');
+  ajax.send();
+  ajax.onreadystatechange = function () {
+    if (ajax.readyState === 4 && ajax.status === 200) {
+      let html = ajax.responseText;
+      console.log("获取页面成功");
+      console.log(html);
+    } else {
+      console.log("获取页面失败")
+    }
+  }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,90 +1,80 @@
 <template>
-  <div class="home">
-    <img style="width: 500px;height: 500px" alt="logo" src="../assets/logo.png">
-    <iframe style="width: 2px;height: 2px"
-            ref="ueIframe"
-            id="ueIframe"
-            @onload="initIframe"
-            :src="baseUrl+'/analystui/?lan=zh#/project'">
+    <div class="home">
+        <img style="width: 500px;height: 500px" alt="logo" src="../assets/logo.png">
+<!--        <iframe style="width: 2px;height: 2px"-->
+<!--                ref="ueIframe"-->
+<!--                id="ueIframe"-->
+<!--                @onload="initIframe"-->
+<!--                :src="baseUrl+'/analystui/?lan=zh#/project'">-->
 
-    </iframe>
-  </div>
+<!--        </iframe>-->
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import {config} from "../utils/config";
-export default {
-  name: 'home',
-  data(){
-    return {
-      baseUrl:config.ueUrl
-    }
-  },
-  mounted() {
-    //<iframe id="iframe" onload='document.getElementById("iframe").src="javascript:$(\"#username\").val(\"admin\");$(\"#_password\").val(\"cdsf@119\");$(\"#cas_submit\").click();"' src="/analystui/?lan=en#/project">
+    // @ is an alias to /src
+    import {config} from "../utils/config";
+    import {loginUe,getUeToken} from "../utils/auth";
 
-    //</iframe>
-    //const ueIframe = this.$refs['ueIframe'];
-    //ueIframe.src = 'javascript:document.getElementById("username").value(\'admin\');console.log(document.getElementById("username"));document.getElementById("_password").val(\'cdsf@119\');document.getElementById("cas_submit").click();'
-    //console.log(ueIframe);
-    const self = this
-    this.$nextTick(() =>{
-      const iframe = document.querySelector('#ueIframe');
-      console.log(iframe);
-      if (iframe.attachEvent) {
-        iframe.attachEvent("onload", function() {
-          console.log('哈哈哈')
-        });
-      } else {
-        iframe.onload = function() {
+    export default {
+        name: 'home',
+        data() {
+            return {
+                baseUrl: config.ueUrl
+            }
+        },
+        mounted() {
+            //<iframe id="iframe" onload='document.getElementById("iframe").src="javascript:$(\"#username\").val(\"admin\");$(\"#_password\").val(\"cdsf@119\");$(\"#cas_submit\").click();"' src="/analystui/?lan=en#/project">
 
-          const d = iframe.contentWindow.document;
-          console.log(d.getElementById("username"));
-          d.getElementById("username").value = "admin";
-          d.getElementById("username").autocomplete="off";
-          d.getElementById("_password").value = "cdsf@119";
-          d.getElementById("_password").autocomplete = "off";
-          d.getElementById("cas_submit").click();
-          //const iframeNode = window.frames["ueIframe"].document;
-        };
-      }
-    })
+            //</iframe>
+            //const ueIframe = this.$refs['ueIframe'];
+            //ueIframe.src = 'javascript:document.getElementById("username").value(\'admin\');console.log(document.getElementById("username"));document.getElementById("_password").val(\'cdsf@119\');document.getElementById("cas_submit").click();'
+            //console.log(ueIframe);
+            // const self = this
+            // this.$nextTick(() => {
+            //     const iframe = document.querySelector('#ueIframe');
+            //     console.log(iframe);
+            //     if (iframe.attachEvent) {
+            //         iframe.attachEvent("onload", function () {
+            //             console.log('哈哈哈')
+            //         });
+            //     } else {
+            //         iframe.onload = function () {
+            //
+            //             const d = iframe.contentWindow.document;
+            //             console.log(d.getElementById("username"));
+            //             d.getElementById("username").value = "admin";
+            //             d.getElementById("username").autocomplete = "off";
+            //             d.getElementById("_password").value = "cdsf@119";
+            //             d.getElementById("_password").autocomplete = "off";
+            //             d.getElementById("cas_submit").click();
+            //             //const iframeNode = window.frames["ueIframe"].document;
+            //         };
+            //     }
+            // })
 
-  },
-  methods:{
-    initIframe(){
-      document.getElementById("username").value("admin");
-      console.log(document.getElementById("username"));
-      document.getElementById("_password").val("cdsf@119");
-      document.getElementById("cas_submit").click();
+        },
+        methods: {
+            initIframe() {
+                document.getElementById("username").value("admin");
+                console.log(document.getElementById("username"));
+                document.getElementById("_password").val("cdsf@119");
+                document.getElementById("cas_submit").click();
+            }
+        },
+        components: {},
+        created() {
+            loginUe();
+            //getUeToken();
+        }
     }
-  },
-  components: {
-
-  },
-  created() {
-    const ajax = new XMLHttpRequest();
-    ajax.open('get','/analystui/?lan=en#/project');
-    ajax.send();
-    ajax.onreadystatechange = function () {
-      if (ajax.readyState===4 &&ajax.status===200) {
-        let html = ajax.responseText;
-        console.log("获取页面成功");
-        console.log(html);
-      }else {
-        console.log("获取页面失败")
-      }
-    }
-    }
-}
 </script>
 
 <style lang="scss" scoped>
-  .home{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: calc(100vh - 150px);
-  }
+    .home {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: calc(100vh - 150px);
+    }
 </style>
