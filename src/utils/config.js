bc566a4b53b78a62bc814c0630864dce9c4140b6
arsenalfaning <765205025@qq.com>
2019-10-20 12:19:19 +0800

    bug fix

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,6 +2,8 @@ export const config ={
     ueUrl:'http://10.101.20.100:8080',
     uePassword: 'a53bf4c1d9be195f807e6ae8ee22f09c',
     baseUrl:'http://10.101.20.100',
+    //ueUrl:'http://47.104.161.130:8080',
+    //uePassword: '319a5d2ce2e52aaec8c09008ee85517f',
     //baseUrl:'http://localhost:8061',
     apiPrefix:'api'
 };
