f866f6db5e08a40d556a06eaa7633879a591b11f
arsenalfaning <765205025@qq.com>
2019-10-21 19:08:28 +0800

    config

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,8 +3,9 @@ export const config ={
     uePassword: 'a53bf4c1d9be195f807e6ae8ee22f09c',
     baseUrl:'http://10.101.20.100',
 
-    //ueUrl:'http://47.104.161.130:8080',
-    //uePassword: '319a5d2ce2e52aaec8c09008ee85517f',
+    // ueUrl:'http://47.104.161.130:8080',
+    // uePassword: '319a5d2ce2e52aaec8c09008ee85517f',
+    // baseUrl:'http://47.104.161.130:8061',
     // baseUrl:'http://localhost:80',
     apiPrefix:'api'
 };
